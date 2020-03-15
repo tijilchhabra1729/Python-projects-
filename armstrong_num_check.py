b1d3a91994f42fe2f3bb1908eeb2b36224fadea0
tijilchhabra1729 <60564583+tijilchhabra1729@users.noreply.github.com>
2020-03-16 02:25:11 +0530

    Update armstrong_num_check.py

--- armstrong_num_check.py
+++ armstrong_num_check.py
@@ -1,4 +1,5 @@
 def cube(input_list):
+    # function to cube the list of digits of the number entered by the user
     cubed_list = []
     for m in input_list:
         m = int(m)
@@ -8,12 +9,15 @@ def cube(input_list):
 
 while 1:
     num = input("Enter a number to check if its a armstrong number:  ")
+    # asking the user to input the number
     try:
         a = len(num)
         digit_list = []
         for i in range(1, a + 1):
+            # for loop to create a list of digits of the number entered by the user
             digit_list.append(num[(i - 1)])
         if sum(cube(digit_list)) == int(num):
+            # checking if the number enteres is an armstrong number or not
             print("Number you have entered is an armstrong number.")
         else:
             print("Number you have entered is not an armstrong number.")
@@ -21,6 +25,8 @@ while 1:
         print("Looks like the value entered its not a number")
     play = input("Enter any alphabet key if u want to play again and quit to exit:  ")
     if play.lower() == 'quit':
+        # exiting the code 
         break
     else:
+        # continuing if the user doesn't want to exit
         pass
